7064da4ce48278a0fcf8a8d84d17f719e98186c7
bimogempar <bimogempar@gmail.com>
2022-01-25 14:32:26 +0700

    done make dashboard

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,16 +4,12 @@ export function middleware(req) {
     const token = req.cookies.token
     const url = req.url
 
-    if (!token && url == 'http://localhost:3000/') {
-        return NextResponse.redirect('/login')
-    }
+    // if (!token && url == 'http://localhost:3000/') {
+    //     return NextResponse.redirect('/login')
+    // }
 
     if (token && url == 'http://localhost:3000/login') {
         return NextResponse.redirect('/')
-    }
-
-    if (!token && url == 'http://localhost:3000/') {
-        return NextResponse.redirect('/login')
     }
 
     if (!token && url == 'http://localhost:3000/register') {
