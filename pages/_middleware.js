3bb51fbcc5c70374552a9d93beade7c33dbc9239
bimogempar <bimogempar@gmail.com>
2022-02-25 22:36:15 +0700

    fix middleware using var url

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,19 +4,19 @@ export function middleware(req) {
     const token = req.cookies.token
     const url = req.url
 
-    if (!token && url == `${req.nextUrl.pathname}/`) {
+    if (!token && url == `${url}/`) {
         return NextResponse.redirect('/login')
     }
 
-    if (token && url == `${req.nextUrl.pathname}/login`) {
+    if (token && url == `${url}/login`) {
         return NextResponse.redirect('/')
     }
 
-    if (!token && url == `${req.nextUrl.pathname}/register`) {
+    if (!token && url == `${url}/register`) {
         return NextResponse.redirect('/login')
     }
 
-    if (!token && url == `${req.nextUrl.pathname}/user`) {
+    if (!token && url == `${url}/user`) {
         return NextResponse.redirect('/login')
     }
 }
