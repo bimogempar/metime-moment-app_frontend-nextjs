620522bbc3d978c8ba3fd2ca15ad2a79b3d982e9
bimogempar <bimogempar@gmail.com>
2022-02-25 23:12:31 +0700

    try just using token in middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,19 +4,23 @@ export default function middleware(req) {
     const token = req.cookies.token
     const url = req.url
 
-    if (!token && url === '/') {
+    if (!token) {
         return NextResponse.redirect('/login')
     }
 
-    if (token && url === '/login') {
-        return NextResponse.redirect('/')
-    }
+    // if (!token && url === '/') {
+    //     return NextResponse.redirect('/login')
+    // }
 
-    if (!token && url === '/logout') {
-        return NextResponse.redirect('/login')
-    }
+    // if (token && url === '/login') {
+    //     return NextResponse.redirect('/')
+    // }
 
-    if (!token && url === '/user') {
-        return NextResponse.redirect('/login')
-    }
+    // if (!token && url === '/logout') {
+    //     return NextResponse.redirect('/login')
+    // }
+
+    // if (!token && url === '/user') {
+    //     return NextResponse.redirect('/login')
+    // }
 }
