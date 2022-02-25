28d3d8f2dbb917376d3df8a31761e890ec8a8131
bimogempar <bimogempar@gmail.com>
2022-02-25 22:15:09 +0700

    try fix middleware without env variable

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,19 +4,19 @@ export function middleware(req) {
     const token = req.cookies.token
     const url = req.url
 
-    if (!token && url == '/') {
+    if (!token && url == 'https://metime-moment-app-frontend-nextjs.vercel.app/') {
         return NextResponse.redirect('/login')
     }
 
-    if (token && url == '/login') {
+    if (token && url == 'https://metime-moment-app-frontend-nextjs.vercel.app/login') {
         return NextResponse.redirect('/')
     }
 
-    if (!token && url == '/register') {
+    if (!token && url == 'https://metime-moment-app-frontend-nextjs.vercel.app/register') {
         return NextResponse.redirect('/login')
     }
 
-    if (!token && url == '/user') {
+    if (!token && url == 'https://metime-moment-app-frontend-nextjs.vercel.app/user') {
         return NextResponse.redirect('/login')
     }
 }
