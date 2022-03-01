23c665abdc179e361534953a4b11d3b816416863
bimogempar <bimogempar@gmail.com>
2022-03-01 21:58:12 +0700

    fix ui error message

--- pages/login.js
+++ pages/login.js
@@ -74,7 +74,7 @@ export default function Home() {
                             <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" name="username" onChange={setValue} />
                             <label className="font-light text-sm text-gray-600 pb-1 block">Password</label>
                             <input autoComplete="on" type="password" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" name="password" onChange={setValue} />
-                            <div className="text-red-500 mb-5">{errorResp}</div>
+                            {errorResp && <p className="text-red-500 text-sm mb-5">{errorResp}</p>}
                             <button type="submit" className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
                                 <span className="inline-block mr-2">Login</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block">
