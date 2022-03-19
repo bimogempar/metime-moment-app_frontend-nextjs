8a60c6976c6c4b012ce0743c367eaf1aa4d32dba
bimogempar <bimogempar@gmail.com>
2022-03-19 09:25:16 +0700

    make new getServerSideProps for handling if token not found

--- pages/testing/[token_reset_pass].js
+++ pages/testing/[token_reset_pass].js
@@ -1,14 +1,35 @@
 import React from 'react'
 
 export async function getServerSideProps(context) {
-    console.log(context.query.token_reset_pass)
+    // console.log(context.query.token_reset_pass)
+    const token_initial_password = context.query.token_reset_pass
+
+    const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/set-pass/${token_initial_password}`, {
+        method: 'GET'
+    })
+    const response = await req.json()
+    // console.log(response)
+
+    if (response.error) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: "/login",
+            },
+        }
+    }
+
     return {
-        props: {}, // will be passed to the page component as props
+        props: {
+            response,
+            token_initial_password
+        }
     }
 }
 
-export default function Testing() {
+export default function Testing({ response, token_initial_password }) {
+    // console.log(response.user)
     return (
-        <div>Testing</div>
+        <div>This token {token_initial_password} for user {response.user.email}</div>
     )
 }
