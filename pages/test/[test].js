7b573045cb7a4711680ecab65b798f9bc528e63a
bimogempar <bimogempar@gmail.com>
2022-04-10 00:02:10 +0700

    testing dynamic ssr on prod dev

--- pages/test/[test].js
+++ pages/test/[test].js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function testDetail() {
+export default function testDetail(props) {
     return (
-        <div>testDetail</div>
+        <div>{props.data}</div>
     )
+}
+
+export async function getServerSideProps(ctx) {
+    const data = ctx.query.test
+    return {
+        props: { data }
+    }
 }
