d0cfa4a82d150fc48579926b9c504ab040e1cbba
bimogempar <bimogempar@gmail.com>
2022-05-03 02:23:03 +0700

    fix edit update details without yup project except users project and progress

--- pages/projects/components/DetailsProject.js
+++ pages/projects/components/DetailsProject.js
@@ -1,31 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect, useContext } from 'react'
 import nookies from 'nookies';
 import axios from 'axios';
-import { BsChevronLeft } from 'react-icons/bs';
+import { BsChevronLeft, BsFillTelephoneOutboundFill, BsTrash, BsCalendarDate } from 'react-icons/bs';
+import { FaRegEdit } from "react-icons/fa";
 import { useRouter } from 'next/router';
 import { useFormik } from 'formik';
 import Layout from '../../../components/Layout';
 import { UserContext } from '../../../components/context/userContext';
+import { TiLocationOutline } from 'react-icons/ti';
+import { BiAddToQueue } from 'react-icons/bi';
+import { FiSend } from 'react-icons/fi';
 
 export default function DetailsProject({ data }) {
-
     const [project, setProject] = useState(data.project);
     const [features, setFeatures] = useState(data.project.features);
+    const [users, setUsers] = useState(data.project.users);
     const [inputClient, setInputClient] = useState(false);
     const [permissions, setPermissions] = useState(false);
 
-
     const userContext = useContext(UserContext)
-    // console.log(userContext.user.role)
+
     useEffect(() => {
         if (userContext.user.role === 3 || userContext.user.role === 2) {
             setPermissions(true)
-        } else if (project.users.filter(f => f.id !== userContext.user.id)) {
-            setPermissions(false)
+        } else {
+            project.users.filter(f => f.id === userContext.user.id).length === 0 ? setPermissions(false) : setPermissions(true)
         }
     }, [project.users, userContext.user.id, userContext.user.role])
-
-    // console.log(project.users.filter(f => f.id == userContext.user.id))
 
     const router = useRouter();
 
@@ -33,7 +35,7 @@ export default function DetailsProject({ data }) {
     const token = cookies.token
 
     const deleteFeature = (feature) => {
-        console.log(feature);
+        // console.log(feature);
         axios.delete(`${process.env.NEXT_PUBLIC_URL}/api/features/${feature}/delete`, {
             headers: {
                 Authorization: 'Bearer ' + token,
@@ -79,8 +81,11 @@ export default function DetailsProject({ data }) {
             client: project.client,
             status: project.status,
             date: project.date,
+            phone_number: project.phone_number,
+            location: project.location,
         },
         onSubmit: values => {
+            console.log(values);
             axios.patch(`${process.env.NEXT_PUBLIC_URL}/api/projects/update/${project.slug}`, values, {
                 headers: {
                     Authorization: 'Bearer ' + token,
@@ -130,75 +135,174 @@ export default function DetailsProject({ data }) {
                 </button>
             </div>
 
-            {/* edit status */}
-            <select name="status" id="status" onChange={formikProjects.handleChange} onChangeCapture={formikProjects.handleSubmit} defaultValue={project.status}>
-                <option value="1">On Scheduled</option>
-                <option value="2">On Progress</option>
-                <option value="3">Done</option>
-            </select>
-
-            {/* edit date */}
-            <label htmlFor="date"></label>
-            <input type="date" id="date" name="date" onChange={formikProjects.handleChange} onBlur={formikProjects.handleSubmit} />
-
-            {/* edit client */}
-            {permissions &&
-                <button onClick={() => { inputClient ? setInputClient(false) : setInputClient(true) }}>Edit</button>
-            }
-            {
-                inputClient ?
-                    <div>
-                        <input type="text" name="client" id="client" value={formikProjects.values.client} onChange={formikProjects.handleChange} />
-                        <button type="button" onClick={formikProjects.handleSubmit}>Simpan</button>
+            <div className="grid grid-cols-3 gap-5 mb-5">
+                <div className="col-span-3 md:col-span-2 bg-white rounded-xl">
+                    <div className="p-5">
+                        <div className="flex items-center justify-between">
+                            {/* Client Name */}
+                            <div className="flex w-2/3 gap-2 text-2xl font-light">
+                                {
+                                    inputClient ?
+                                        <div className="flex w-full gap-2">
+                                            <input className="p-2 bg-gray-100 w-full rounded-lg font-light" type="text" name="client" id="client" value={formikProjects.values.client} onChange={formikProjects.handleChange} />
+                                        </div>
+                                        :
+                                        <h1>{project.client}</h1>
+                                }
+                            </div>
+                            {/* Status */}
+                            <div className="flex items-center gap-2">
+                                <select className="p-2 bg-gray-200 text-gray-500 rounded-lg appearance-none" name="status" id="status" onChange={formikProjects.handleChange} onChangeCapture={formikProjects.handleSubmit} defaultValue={project.status}>
+                                    <option value="1">On Scheduled</option>
+                                    <option value="2">On Progress</option>
+                                    <option value="3">Done</option>
+                                </select>
+                                {/* Button Edit or Submit */}
+                                {inputClient ?
+                                    <button className="bg-blue-500 p-3 rounded-lg text-white" type="button" onClick={formikProjects.handleSubmit}><FiSend /></button>
+                                    :
+                                    permissions ?
+                                        <button onClick={() => { inputClient ? setInputClient(false) : setInputClient(true) }} className="bg-yellow-400 text-white p-2 rounded-lg"><FaRegEdit size={20} /></button>
+                                        : null
+                                }
+                            </div>
+                        </div>
+                        {/* Phone Number */}
+                        {
+                            inputClient ?
+                                <div className="flex items-center gap-2 text-sm text-gray-500">
+                                    <BsFillTelephoneOutboundFill /><input className="p-2 bg-gray-100 w-1/3 mt-2 rounded-lg" type="text" name="phone_number" id="phone_number" value={formikProjects.values.phone_number} onChange={formikProjects.handleChange} />
+                                </div>
+                                :
+                                <p className="text-sm text-gray-500 mt-2 flex items-center gap-3"><BsFillTelephoneOutboundFill /> {project.phone_number}</p>
+                        }
                     </div>
-                    :
-                    <h1>{project.client}</h1>
-            }
 
-            {
-                permissions ?
-                    features.map((feature) => {
-                        return (
-                            <div key={feature.id}>
-                                <input
-                                    type="checkbox"
-                                    id={feature.id}
-                                    name={feature.feature}
-                                    value={feature.feature}
-                                    defaultChecked={feature.status === 1}
-                                    onChange={() => handleClickCB(feature)}
-                                />
-                                {feature.feature}
-                                <button className="mx-5 text-red-500" onClick={() => deleteFeature(feature.id)}>Delete</button>
-                            </div>
-                        )
-                    }) :
-                    features.map((feature) => {
-                        return (
-                            <div key={feature.id}>
-                                <input
-                                    type="checkbox"
-                                    id={feature.id}
-                                    name={feature.feature}
-                                    value={feature.feature}
-                                    defaultChecked={feature.status === 1}
-                                    onChange={() => handleClickCB(feature)}
-                                    disabled
-                                />
-                                {feature.feature}
-                            </div>
-                        )
-                    })
-            }
+                    {/* Image Header */}
+                    <h1 className="p-5 text-2xl font-light">
+                        This is for image header
+                    </h1>
 
-            {/* add new features */}
-            {permissions &&
-                <form onSubmit={formikFeatures.handleSubmit}>
-                    <label htmlFor="features"></label>
-                    <input type="text" id="features" name="feature" placeholder="New feature..." value={formikFeatures.values.feature} onChange={formikFeatures.handleChange} />
-                    <button type="submit">Simpan</button>
-                </form>
-            }
+                    <div className="p-5 grid grid-cols-2 gap-3 justify-items-start">
+                        <div>
+                            {/* Date */}
+                            <h2 className="text-sm font-light text-gray-500 uppercase">Date</h2>
+                            {
+                                inputClient ?
+                                    <input className="text-sm p-2 bg-gray-200 text-gray-600 rounded-lg mt-2" type="date" id="date" name="date" defaultValue={project.date} onChange={formikProjects.handleChange} onBlur={formikProjects.handleSubmit} />
+                                    :
+                                    <p className="flex items-center gap-2 text-sm p-2 bg-gray-200 text-gray-600 rounded-lg mt-2">{project.date} <BsCalendarDate /></p>
+                            }
+                        </div>
+                        <div>
+                            <h2 className="text-sm font-light text-gray-500 uppercase">Team</h2>
+                            {
+                                users.map(user => {
+                                    return (
+                                        <img key={user.id} className="relative z-1 inline object-cover w-8 h-8 border-2 border-white rounded-full mt-2" src="../../../img/ade.png" alt="Profile image" />
+                                    )
+                                })
+                            }
+                        </div>
+                        <div>
+                            {/* Location */}
+                            <h2 className="text-sm font-light text-gray-500 uppercase">Location</h2>
+                            {
+                                inputClient ?
+                                    <div className="flex gap-2 items-center">
+                                        <TiLocationOutline className="text-2xl text-gray-500" /><input className="p-2 bg-gray-100 w-full text-gray-500 mt-2 rounded-lg" type="text" name="location" id="location" value={formikProjects.values.location} onChange={formikProjects.handleChange} />
+                                    </div>
+                                    :
+                                    <h2 className="text-sm p-2 text-gray-600 rounded-lg mt-2 flex items-center gap-2"><TiLocationOutline className="text-lg" /> {project.location}</h2>
+                            }
+                        </div>
+                    </div>
+
+                    {/* Chekclist */}
+                    <div className="p-5">
+                        <h1 className="text-gray-600 text-2xl font-extralight">
+                            Checklist
+                        </h1>
+                        {permissions ? features.map((feature) => {
+                            return (
+                                <div className="mt-3 gap-3" key={feature.id}>
+                                    <div className="flex text-gray-600 items-center gap-3 bg-gray-100 p-2 px-4 inline-flex rounded-lg">
+                                        <input
+                                            type="checkbox"
+                                            id={feature.id}
+                                            name={feature.feature}
+                                            value={feature.feature}
+                                            defaultChecked={feature.status === 1}
+                                            onChange={() => handleClickCB(feature)}
+                                        />
+                                        <p>{feature.feature}</p>
+                                        <button type="button" className="ml-5" onClick={() => deleteFeature(feature.id)}><BsTrash /></button>
+                                    </div>
+                                </div>
+                            )
+                        }) :
+                            features.map((feature) => {
+                                return (
+                                    <div className="mt-3 gap-3" key={feature.id}>
+                                        <div className="flex text-gray-600 items-center gap-3 bg-gray-100 p-2 px-4 inline-flex rounded-lg">
+                                            <input
+                                                type="checkbox"
+                                                id={feature.id}
+                                                name={feature.feature}
+                                                value={feature.feature}
+                                                defaultChecked={feature.status === 1}
+                                                onChange={() => handleClickCB(feature)}
+                                                disabled
+                                            />
+                                            <p>{feature.feature}</p>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        {permissions &&
+                            <form onSubmit={formikFeatures.handleSubmit}>
+                                <div className="flex items-center mt-3 gap-3">
+                                    <input id="features" name="feature" value={formikFeatures.values.feature} onChange={formikFeatures.handleChange} type="text" placeholder="Add new feature" className="p-2 bg-gray-100 rounded-lg w-full sm:w-1/2 md:w-1/2" />
+                                    <button type="submit" className="bg-blue-500 p-3 rounded-lg"> <BiAddToQueue className="text-white" /> </button>
+                                </div>
+                            </form>
+                        }
+                    </div>
+
+                    <div className="p-5">
+                        <h1 className="text-gray-600 text-2xl font-extralight">
+                            Comment
+                        </h1>
+                        <div className="flex gap-3 items-start mt-3">
+                            <img className="relative z-1 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="../../../img/ade.png" alt="Profile image" />
+                            <div>
+                                <div>
+                                    <textarea className="bg-gray-100 rounded-lg p-3 text-gray-600 w-full md:w-7/8" placeholder="Input your progress..." cols="50" name="" id="" />
+                                </div>
+                                <div className="flex justify-end mt-1">
+                                    <button className="bg-green-500 p-2 rounded-lg flex items-center gap-2 text-white text-sm"> <FiSend /> Send</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex w-3/4 gap-3 items-start mt-4">
+                            <img className="relative z-1 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="../../../img/ade.png" alt="Profile image" />
+                            <div className="g">
+                                <div className="flex justify-between items-center">
+                                    <h4 className="text-gray-700">Ade Novan Guliaano</h4>
+                                    <h4 className="text-sm text-gray-500">4 min ago</h4>
+                                </div>
+                                <p className="text-gray-500 text-sm mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae temporibus ex perspiciatis? Nemo blanditiis quasi commodi obcaecati facere, quia unde.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="col-span-2 md:col-span-1 sm:col-span-2 rounded-xl">
+                    <div className="p-5 bg-white rounded-xl">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque, atque?
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
