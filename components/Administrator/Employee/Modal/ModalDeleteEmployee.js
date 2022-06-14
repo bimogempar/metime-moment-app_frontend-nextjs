3373941ffce90c02cc609ac0e71e9f9678b46d76
bimogempar <bimogempar@gmail.com>
2022-06-14 19:33:08 +0700

    style: fix modaldeleteemployee

--- components/Administrator/Employee/Modal/ModalDeleteEmployee.js
+++ components/Administrator/Employee/Modal/ModalDeleteEmployee.js
@@ -26,12 +26,17 @@ export default function ModalDeleteEmployee({ setIsOpen, isOpen, buttonRef, data
                     leaveTo="transform scale-95 opacity-0"
                 >
                     <div className="flex items-center justify-center min-h-screen">
-                        <div className="relative bg-white rounded max-w-3xl md:mx-auto p-5 md:my-5 m-5">
-                            {title}
-                            <p>
-                                Yakin ingin menghapus user {data == undefined ? '' : data.name} ?
-                            </p>
-                            <button className='bg-blue-500 text-white py-2 px-3 rounded-lg' onClick={e => deleteEmployee(data.id)}>Hapus</button>
+                        <div className="relative w-1/2 xl:w-1/3 bg-white my-5 rounded-md p-4">
+                            <div className="grid grid-cols-1 gap-3">
+                                <div className="flex justify-between">
+                                    <h1 className="text-gray-700 font-light text-2xl">{title}</h1>
+                                </div>
+                                <hr />
+                                <p className="text-gray-700 font-light text-md">Yakin ingin menghapus user  <span className="font-medium">{data == undefined ? '' : data.name}</span> ?</p>
+                                <div className="flex justify-end">
+                                    <button className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-3 rounded-lg' onClick={e => deleteEmployee(data.id)}>Hapus</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </Transition.Child>
